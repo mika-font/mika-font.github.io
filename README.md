477bba4314400418588f5acf5610404a3200410b
Mikael Fontoura <mikaelfontoura29@gmail.com>
2026-01-22 12:24:04 -0300

    initial project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mika-font.github.io
+
 Welcome to my professional portfolio. Here you can explore my projects, technical skills, and academic background.
---
+++ script.js
@@ -0,0 +1,3 @@
+direitos = document.getElementById("direitos");
+anoAtual = new Date().getFullYear();
+direitos.innerHTML = `&copy; ${anoAtual}. Mikael Fontoura do Nascimento. Todos os direitos reservados.`;
